529613619693abb7b46a7504357012addff07202
yigitoner28 <yigitoner28@gmail.com>
2021-10-29 23:54:58 +0300

    Initial commit

---
+++ src/com/medipol/odev/ikisayiyitoplamaodevi.java
@@ -0,0 +1,12 @@
+package com.medipol.odev;
+
+public class ikisayiyitoplamaodevi {
+
+	public static void main(String[] args) {
+	int a = 20;
+	int b = 44;
+	System.out.println(a+b);
+
+	}
+
+}
---
+++ src/com/medipol/operators/AritmatikOperator.java
@@ -0,0 +1,11 @@
+package com.medipol.operators;
+
+public class AritmatikOperator {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+		
+		
+	}
+
+}
