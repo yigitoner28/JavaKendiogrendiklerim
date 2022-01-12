3a3d29f107767afa6c60ee2b129233a21e9ea264
yigitoner28 <93399813+yigitoner28@users.noreply.github.com>
2022-01-12 12:04:55 +0300

    Add files via upload
    enBuyukElemaniBul

---
+++ src/com/medipol/odev/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    public static void main(String[] arguments) {
+        double[][] matris = {{56, 12, 1, 231},
+                {234, 21, 78, 26},
+                {654, 33, 32, 67},
+                {189, 35, 56, 89}
+
+        };
+
+
+
+    }
+}
